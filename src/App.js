82b3b0188aedd353abfaad045d2a721bae60eceb
Harpreet <singh.harpreet4499@gmail.com>
2020-09-03 19:37:54 +0530

    Updating app with collection item

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {Homepage} from './pages/homepage/homepage.component';
 
 import {Switch,Route} from 'react-router-dom';
+import ShopPage from './components/shop/shop.component';
 
 
 const HatsPage = () => (
@@ -16,7 +17,7 @@ function App() {
       {/* <Homepage /> */}
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     
 
